0f86a4a1937c9f5ba623ee14519ca4e04286739c
Phil Prasek <prasek@gmail.com>
2021-06-06 08:51:54 -0700

    query format
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- .scripts/query.sh
+++ .scripts/query.sh
@@ -4,6 +4,7 @@ PORT="${1:-4000}"
 
 CURL="curl -X POST -H \"Content-Type: application/json\" --data '{ \"query\": \"{ bestSellers { title } } \" }' http://localhost:$PORT/"
 
+echo -------------------------------------------------------------------------------------------
 echo $CURL
-
 bash -c "$CURL"
+echo -------------------------------------------------------------------------------------------
