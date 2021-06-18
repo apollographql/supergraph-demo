ae2b0d1ca00bf41d8bfae61587e855ec5da17d16
Phil Prasek <prasek@gmail.com>
2021-06-17 20:23:38 -0700

    docs update
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- .scripts/check-products.sh
+++ .scripts/check-products.sh
@@ -5,8 +5,7 @@ source "$(dirname $0)/get-env.sh"
 
 graph=$1
 if [[ -z "${graph}" ]]; then
-  source "$(dirname $0)/get-graph-id.sh"
+  source "$(dirname $0)/get-graph-ref.sh"
 fi
 
-echo "rover subgraph check ${graph} --schema subgraphs/products.graphql --name products"
-rover subgraph check ${graph} --schema subgraphs/products.graphql --name products
+( set -x; rover subgraph check ${graph} --schema subgraphs/products/products.graphql --name products )
