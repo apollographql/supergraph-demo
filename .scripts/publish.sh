a16431ab3b4ecff0765eb977e0b15334671c54e6
Phil Prasek <prasek@gmail.com>
2024-01-24 15:34:41 -0800

    fix rover error handling
    Signed-off-by: Phil Prasek <prasek@gmail.com>

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -1,17 +1,25 @@
 #!/bin/bash 
 
 echo "======================================="
-echo "SUBGRAPH PUBLISH"
+echo "PUBLISH SUBGRAPHS TO APOLLO REGISTRY"
 echo "======================================="
 
 source "$(dirname $0)/subgraphs.sh"
 source "$(dirname $0)/graph-api-env.sh"
+
+# note: use --allow-invalid-routing-url to allow localhost without confirmation prompt
 
 for subgraph in ${subgraphs[@]}; do
   echo "---------------------------------------"
   echo "subgraph: ${subgraph}"
   echo "---------------------------------------"
   url="url_$subgraph"
-  (set -x; ${ROVER_BIN:-'rover'} subgraph publish ${APOLLO_GRAPH_REF} --routing-url "${!url}" --schema subgraphs/${subgraph}/${subgraph}.graphql --name ${subgraph} --convert)
+  schema="subgraphs/$subgraph/$subgraph.graphql"
+  (set -x; ${ROVER_BIN:-'rover'} subgraph publish ${APOLLO_GRAPH_REF} \
+    --routing-url "${!url}" \
+    --schema "${schema}" \
+    --name ${subgraph} \
+    --allow-invalid-routing-url \
+    --convert)
   echo ""
 done
